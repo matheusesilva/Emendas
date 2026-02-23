2ab260295e2430018ad8b26b02ba5e55300984c0
Matheus Silva <sdeduardo.cro2@gmail.com>
2026-02-23 12:11:34 -0300

    feat: add Timer class for time tracking

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import yaml
 import logging
+import time
 from typing import Dict, Set, Tuple, List, Any
 from pyspark.sql import DataFrame
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DoubleType, LongType
@@ -18,36 +19,33 @@ def get_logger(name: str) -> logging.Logger:
     if not logger.hasHandlers():
         log_config = load_config().get('logging', {})
         level_str = log_config.get('level', 'INFO').upper()
-        level = getattr(logging, level_str, logging.INFO)
-        fmt = log_config.get('format', '%(asctime)s | %(levelname)-8s | %(message)s')
+        level = getattr(logging, level_str)
+        fmt = log_config.get('format')
         logging.basicConfig(
             level=level,
             format=fmt
         )
     return logger
 
-def load_config(config_path: str = "config.yaml", args=None) -> Dict[str, Any]:
+def load_config(args=None) -> Dict[str, Any]:
     """Carrega a configuração do arquivo YAML e atualiza com argumentos CLI se fornecidos."""
-    with open(config_path, "r") as f:
-        config = yaml.safe_load(f)
+    try:
+        config_path = args.config # Default é 'config.yaml' se não for fornecido via CLI
+        with open(config_path, "r") as f:
+            config = yaml.safe_load(f)
+    except Exception as e:
+        logging.error(f"CONFIG    | Falha ao carregar {config_path}: {str(e)}")
+        raise Exception(f"Falha ao carregar configuração: {str(e)}")
+    
+    # Sobrescreve configurações com argumentos CLI, se fornecidos
     if args is not None:
-        # Atualiza os caminhos das layers se fornecidos
-        if getattr(args, 'pipeline', None):
-            config['default_pipeline'] = args.pipeline
-        if getattr(args, 'raw_path', None):
-            config['storage']['raw'] = args.raw_path
-        if getattr(args, 'bronze_path', None):
-            config['storage']['bronze'] = args.bronze_path
-        if getattr(args, 'silver_path', None):
-            config['storage']['silver'] = args.silver_path
-        if getattr(args, 'gold_path', None):
-            config['storage']['gold'] = args.gold_path
-        # Atualiza URL de download se fornecida
-        if getattr(args, 'url', None):
-            config['pipelines']['emendas_parlamentares']['url'] = args.url
-        # Atualiza nível de logging se fornecido
-        if getattr(args, 'log_level', None):
-            config['logging']['level'] = args.log_level.upper()
+        if args.pipeline: config['default_pipeline'] = args.pipeline
+        if args.raw_path: config['storage']['raw'] = args.raw_path
+        if args.bronze_path: config['storage']['bronze'] = args.bronze_path
+        if args.silver_path: config['storage']['silver'] = args.silver_path
+        if args.gold_path: config['storage']['gold'] = args.gold_path
+        if args.url: config['pipelines']['emendas_parlamentares']['url'] = args.url
+        if args.log_level: config['logging']['level'] = args.log_level.upper()
     return config
 
 def build_spark_schema(yaml_schema: List[Dict[str, Any]]) -> StructType:
@@ -67,3 +65,16 @@ def check_schema_consistency(df: DataFrame, expected_schema: StructType) -> Tupl
     new_columns: Set[str] = df_columns - expected_columns
     
     return missing_columns, new_columns
+
+class Timer:
+    """Context manager para medir o tempo de execução de blocos de código."""
+    def __init__(self):
+        self.start_time: float = 0.0
+        self.end_time: float = 0.0
+
+    def start (self):
+        self.start_time = time.time()
+    
+    def duration(self):
+        self.end_time = time.time()
+        return self.end_time - self.start_time
