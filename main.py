3c391115603c9bfb5f4ae8c4a1ef358dd5704fb2
Matheus Silva <sdeduardo.cro2@gmail.com>
2026-02-23 12:10:04 -0300

    refactor: add new support functions for clean code

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import argparse
 from src import ETLPipeline
-from src.utils import get_logger, load_config
-
 
 if __name__ == "__main__":
+
+    # Configurações de argumentos para permitir flexibilidade na execução do pipeline
     parser = argparse.ArgumentParser(description="Executa o pipeline ETL")
     parser.add_argument('--pipeline', type=str, help='Nome da pipeline a ser executada')
     parser.add_argument('--raw-path', type=str, help='Caminho para a camada raw')
@@ -12,12 +12,8 @@ if __name__ == "__main__":
     parser.add_argument('--gold-path', type=str, help='Caminho para a camada gold')
     parser.add_argument('--url', type=str, help='URL para download dos dados')
     parser.add_argument('--log-level', type=str, help='Nível de logging (ex: INFO, DEBUG, WARNING)')
+    parser.add_argument('--config', type=str, help='Nome do arquivo de configuração YAML', default='config.yaml')
     args = parser.parse_args()
 
-    config = load_config(args=args)
-
-    logger = get_logger("Main")
-
-    pipeline = ETLPipeline(config.get('default_pipeline'))
-    pipeline.run_config = config 
+    pipeline = ETLPipeline(args=args)
     pipeline.run()
